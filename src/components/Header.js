2b360375730827b3d90376ee9d032326c0e5c750
Clawyer <60699067+Clawyer@users.noreply.github.com>
2022-03-31 16:20:14 +0200

    init header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,29 @@
-import React from "react"
-import logo from "../assets/img/logo.jpg"
-import "../assets/css/style.css";
+import React from "react";
+import logo from "../assets/img/logo.jpg";
+import "../assets/css/header.css";
 
-export default function Header(){
-    return (
-        <nav className="nav">
-            <img className="logo" src={logo} alt="logo" />
-            <ul className="menu">
-                <li> <a href="#">Starters</a> </li>
-                <li> <a href="#"> Main </a> </li>
-                <li> <a href="#"> Dessert </a> </li>
-            </ul>
-        </nav>
-    )
+export default function Header() {
+  return (
+    <header className="header">
+      {" "}
+      <nav className="nav">
+        <img className="logo" src={logo} alt="logo" />
+        <h1>JM - CUISINE </h1>
+        <ul className="menu">
+          <li>
+            {" "}
+            <a href="#">Starters</a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a href="#"> Main </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a href="#"> Dessert </a>{" "}
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
