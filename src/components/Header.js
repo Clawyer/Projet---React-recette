45cecde4528c4e82a0c568458b7820e8a718a139
Clawyer <60699067+Clawyer@users.noreply.github.com>
2022-04-01 10:25:22 +0200

    fix header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,4 @@
 import React from "react"
-// import { Carousel } from "react-carousel-minimal";
-import cars_1 from "../assets/img/carousel_img1.jpg"
-import cars_2 from "../assets/img/carousel_img2.jpg"
-import cars_3 from "../assets/img/carousel_img3.jpg"
-import cars_4 from "../assets/img/carousel_img4.jpg"
-import cars_5 from "../assets/img/carousel_img5.jpg"
 import "../assets/css/header.css"
 
 export default function Header() {
