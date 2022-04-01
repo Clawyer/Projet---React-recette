ebed70d45476bf974c7c3a38b33450e82ea20c87
Clawyer <60699067+Clawyer@users.noreply.github.com>
2022-04-01 19:00:04 +0200

    create starters pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
-// import "./App.css"
+import Starters from "./pages/Starters";
+import "./App.css"
 
 function App() {
   return (
     <>
     <Header />
+    <Starters />
     </>
   );
 }
--- src/pages/Starters.js
+++ src/pages/Starters.js
@@ -1,1 +1,11 @@
-//importer le starters.css
+import React from "react";
+import "../assets/css/starters.css";
+import img from "../data/images";
+import Dish from "../components/Dish";
+
+export default function Starters(props) {
+  const images = img.images.map((x) => {
+    return <Dish key={x.title} {...x} />;
+  });
+  return <section className="starters">{images}</section>;
+}
