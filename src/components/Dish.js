41ee59c53ecd50284f8c22f34654b788dc8621da
Clawyer <60699067+Clawyer@users.noreply.github.com>
2022-04-04 13:45:42 +0200

    add router

--- src/components/Dish.js
+++ src/components/Dish.js
@@ -2,8 +2,9 @@ import React from "react";
 import "../assets/css/dishes.css";
 
 export default function Dish(props) {
+
   return (
-    <div className="dish">
+    <div  className={`dish ${props.title}`}>
       <img className="dish_img" src={props.url} alt={props.title} />
       <h3 className="dish--title">{props.title}</h3>
       <p>{props.description}</p>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,43 @@
-import React from "react"
-import "../assets/css/header.css"
-import logo from "../assets/img/logo.png"
+import React from "react";
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import "../assets/css/header.css";
+import logo from "../assets/img/logo.png";
+import Home from "../pages/Home";
+import Main from "../pages/Main";
+import Starters from "../pages/Starters";
+import Desserts from "../pages/Desserts";
+import Details from "../pages/Details";
 
 export default function Header() {
-
   return (
-    <header className="header">
-      <nav className="nav">
-        <img className="logo" src={logo} alt="JM factory" />
-        <h1 className="header--title">JM - CUISINE </h1>
-        <ul className="menu">
-          <li>
-            <a href="#">Top dishes</a>
-          </li>
-          <li>
-            <a href="#">Starters</a>
-          </li>
-          <li>
-            <a href="#"> Main </a>
-          </li>
-          <li>
-            <a href="#"> Dessert </a>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="starters/*" element={<Starters />} />
+        <Route path="main_dish/*" element={<Main />} />
+        <Route path="desserts/*" element={<Desserts />} />
+        <Route path="/details/:id" element={<Details />} />
+      </Routes>
+      <header className="header">
+        <nav className="nav">
+          <img className="logo" src={logo} alt="JM factory" />
+          <h1 className="header--title">JM - CUISINE </h1>
+          <ul className="menu">
+            <li>
+              <Link to="/">Top dishes</Link>
+            </li>
+            <li>
+              <Link to="/starters">Starters</Link>
+            </li>
+            <li>
+              <Link to="/desserts"> Main</Link>
+            </li>
+            <li>
+              <Link to="/main_dish"> Dessert</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </Router>
   );
 }
