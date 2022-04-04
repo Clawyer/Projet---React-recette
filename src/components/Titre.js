4621c31cefa968caca454e08a67980129ad53a2e
Clawyer <60699067+Clawyer@users.noreply.github.com>
2022-04-04 12:44:05 +0200

    fix css + background

--- src/components/Titre.js
+++ src/components/Titre.js
@@ -2,9 +2,8 @@ import React from "react";
 export default function Titre(props) {
 return (
   <div className="desserts">
-    <h2 className="desserts-title">{props.title}</h2>
     <img className="desserts-img" src={props.url} alt={props.title}></img>
+    <h2 className="desserts-title">{props.title}</h2>
   </div>
 );
 }
-
