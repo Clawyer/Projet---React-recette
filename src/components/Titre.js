7754e7131839cf77a0fe9587260eeec882c611a0
Inoriex <inoriex@gmail.com>
2022-04-04 10:19:07 +0200

    props fini

--- src/components/Titre.js
+++ src/components/Titre.js
@@ -1,7 +1,10 @@
 import React from "react";
 export default function Titre(props) {
-  <figure>
+return (
+  <div>
     <h2>{props.title}</h2>
     <img className="desserts" src={props.url} alt={props.title}></img>
-  </figure>;
+  </div>
+)
 }
+
