1e36a140c4be38622ef1ea508402519876dd34a8
Clawyer <60699067+Clawyer@users.noreply.github.com>
2022-04-04 10:37:38 +0200

    init main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import Main from "./pages/Main"
 import "./App.css"
 
 function App() {
   return (
     <>
     <Header />
+    <Main />
     </>
   );
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,1 +1,14 @@
-//importer le main.css
+import data_main from "../data/data_main"
+import Dish from "../components/Dish"
+import "../assets/css/main.css"
+
+export default function Main() {
+    const main = data_main.data.map((x) => {
+        return <Dish key={x.title} {...x} />
+    })
+    return (
+        <>
+           {main}
+        </>
+    )
+}
