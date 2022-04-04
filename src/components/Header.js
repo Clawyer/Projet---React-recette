88f53da0ccf8fdbc14a921349295d421ebbdc7b0
Clawyer <60699067+Clawyer@users.noreply.github.com>
2022-04-04 12:50:33 +0200

    fix css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React from "react"
 import "../assets/css/header.css"
-import "../assets/css/style.css"
 import logo from "../assets/img/logo.png"
 
 export default function Header() {
@@ -11,6 +10,9 @@ export default function Header() {
         <img className="logo" src={logo} alt="JM factory" />
         <h1 className="header--title">JM - CUISINE </h1>
         <ul className="menu">
+          <li>
+            <a href="#">Top dishes</a>
+          </li>
           <li>
             <a href="#">Starters</a>
           </li>
